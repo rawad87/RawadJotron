26a6112ea1d76d13e0639d57c6b2025912575e2a
GET Academy <terje@kolderup.net>
2020-05-13 12:02:25 +0200

    Initial commit

---
+++ js/dataAccess.js
@@ -0,0 +1,55 @@
+const dataAccess = {
+    readPsTemplate: async function () {
+        return await this.readXmlAjaxServer('readPSTemplate', '', '');
+    },
+
+    readParagraphTemplate: async function () {
+        return await this.readXmlAjaxServer('readParagraphTemplate', '', '');
+    },
+
+    readPS: async function (docnum) {
+        return await this.readXmlAjaxServer('readPS', docnum, '')
+    },
+
+    readPSList: async function () {
+        return await this.readXmlAjaxServer('readPSList', '', '');
+    },
+
+    saveNewPS: async function () {
+        return await this.readXmlAjaxServer('saveNewPS', '', '');
+    },
+
+    readXmlAjaxServer: async function (subject, name, data) {
+        return await $.post("ps_ajax_server.php", { subject, name, data });
+    },
+
+    objectFromXmlText: function (xmlText) {
+        let dom = this.parseXml(xmlText);
+        let json = xml2json(dom).replace('undefined', '');
+        return JSON.parse(json);
+    },
+
+    parseXml: function (xml) {
+        var dom = null;
+        if (window.DOMParser) {
+            try {
+                dom = (new DOMParser()).parseFromString(xml, "text/xml");
+            }
+            catch (e) { dom = null; }
+        }
+        else if (window.ActiveXObject) {
+            try {
+                dom = new ActiveXObject('Microsoft.XMLDOM');
+                dom.async = false;
+                if (!dom.loadXML(xml)) // parse error ..
+
+                    window.alert(dom.parseError.reason + dom.parseError.srcText);
+            }
+            catch (e) { dom = null; }
+        }
+        else
+            alert("cannot parse xml string!");
+        return dom;
+    },
+}
+
